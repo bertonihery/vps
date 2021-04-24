f47693b59b9decf193eb6615ecbf660d40ac712e
bertonihery <83114127+bertonihery@users.noreply.github.com>
2021-04-24 15:49:08 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -3,7 +3,6 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
 tar xf xmrig-6.12.1-linux-x64.tar.gz
-cd xmrig-6.12.1
 ./xmrig -o pool.hashvault.pro:80 -u 88Ekeyyq4z4RSXPJLMW847VBtJ7RoADiaQn74Hq26PefRZSX1rbBZef9rY5MTy4b2TUAF85za7Jw9ZJWThwtWCKnBwaJphQ -p vpsgithub --donate-level 1 --threads=2
 while [1]; do
 sleep 3
